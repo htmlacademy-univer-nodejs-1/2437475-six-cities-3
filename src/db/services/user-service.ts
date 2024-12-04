b819b605198756134baa90a53fca2e0576d72e2a
SoftOwl256 <anna.sok.03@mail.ru>
2024-12-04 22:49:10 +0500

    done??

--- src/db/services/user-service.ts
+++ src/db/services/user-service.ts
@@ -2,10 +2,11 @@ import UserModel from '../models/user.js';
 import { User } from '../../types/types.js';
 import { CreateUserDTO, LoginDTO } from '../dto/user.dto.js';
 import bcrypt from 'bcrypt';
+import { EntityService } from './entity-service.js';
 
 const SALT_ROUNDS = 10;
 
-class UserService {
+class UserService implements EntityService {
   async createUser(data: CreateUserDTO): Promise<User> {
     const hashedPassword = await bcrypt.hash(data.password, SALT_ROUNDS);
     const newUser = await UserModel.create({
@@ -15,12 +16,20 @@ class UserService {
     return newUser as User;
   }
 
+  async findById(id: string): Promise<User | null> {
+    return this.findUserById(id);
+  }
+
   async findUserById(id: string): Promise<User | null> {
     return UserModel.findById(id).exec() as Promise<User | null>;
   }
 
   async findUserByEmail(email: string): Promise<User | null> {
     return UserModel.findOne({ email }).exec() as Promise<User | null>;
+  }
+
+  async updateUserAvatar(userId: string, avatarPath: string): Promise<any> {
+    return UserModel.findByIdAndUpdate(userId, { avatar: avatarPath }, { new: true }).exec();
   }
 
   async authenticateUser(data: LoginDTO): Promise<User | null> {
---
+++ src/rest/middleware/upload-avatar-middleware.ts
@@ -0,0 +1,35 @@
+import multer from 'multer';
+import { Request } from 'express';
+import { nanoid } from 'nanoid';
+import mime from 'mime-types';
+import path from 'path';
+
+const uploadsDir = process.env.UPLOADS_DIR || './uploads';
+
+const storage = multer.diskStorage({
+  destination: (_req, _file, cb) => {
+    cb(null, path.resolve(uploadsDir, 'avatars'));
+  },
+  filename: (_req, file, cb) => {
+    const ext = mime.extension(file.mimetype);
+    const uniqueName = `${nanoid()}.${ext}`;
+    cb(null, uniqueName);
+  },
+});
+
+const fileFilter = (_req: Request, file: Express.Multer.File, cb: multer.FileFilterCallback) => {
+  const allowedMimeTypes = ['image/jpeg', 'image/png'];
+  if (allowedMimeTypes.includes(file.mimetype)) {
+    cb(null, true); // Разрешаем загрузку
+  } else {
+    cb(new Error('Invalid file type. Only JPEG, PNG, and GIF are allowed.'));
+  }
+};
+
+const upload = multer({
+  storage,
+  fileFilter,
+  limits: { fileSize: 5 * 1024 * 1024 }, // 5 MB
+});
+
+export const uploadAvatar = upload.single('avatar');
